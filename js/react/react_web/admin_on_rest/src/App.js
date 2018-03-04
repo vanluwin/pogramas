d91d3d4ac81287bc79e7045269b1795ce1d87c7c
richard <richardsonsantiago@ufrn.edu.br>
2018-03-04 00:38:22 -0300

    admin_on_rest Tutorial

---
+++ js/react/react_web/admin_on_rest/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { jsonServerRestClient, Admin, Resource, Delete } from 'admin-on-rest';
+
+import { PostList, PostEdit, PostCreate } from './posts';
+import { UserList } from './users';
+
+import PostIcon from 'material-ui/svg-icons/action/book';
+import UserIcon from 'material-ui/svg-icons/social/group';
+
+import Dashboard from './Dashboard';
+
+import authClient from './authClient';
+
+const App = () => (
+    <Admin authClient={authClient}  dashboard={Dashboard} restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} remove={Delete} icon={PostIcon}/>
+        <Resource name="users" list={UserList} icon={UserIcon} />
+    </Admin>
+);
+
+export default App;
